2ee2aee5665c057e0596f56bb30046e8ee5616f9
eduardogz94 <ezangarini@hotmail.com>
2018-07-01 19:28:54 -0400

    Added new layout

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -28,8 +28,11 @@ export class LoginPage {
     private session: SessionService
   ) {
     this.user = this.formBuilder.group({
-      username: [''],
-      password: ['']
+      log_username: [''],
+      log_password: [''],
+      sign_username: [''],
+      sign_password: [''],
+      sign_password_confirmation: ['']
     })
   }
 
@@ -38,10 +41,11 @@ export class LoginPage {
   }
 
   logForm() {
-    const { username, password } = this.user.value
-    console.log(username, password)
-    this.nativeStorage.getItem(username).then(data => {
-      if (password == data.password) {
+    const { log_username, log_password } = this.user.value
+    console.log(log_username, log_password)
+    this.nativeStorage.getItem(log_username).then(data => {
+      console.log(data)
+      if (log_password == data.sign_password) {
         let alert = this.alertCtrl.create({
           title: 'Log in successful',
           subTitle: 'You have logged! Welcome',
@@ -49,9 +53,9 @@ export class LoginPage {
           })
           alert.present();
           alert.onDidDismiss(() => {
-            this.session.setUser(username)
+            this.session.setUser(log_username)
             this.navCtrl.setRoot(HomePage, {
-              username
+              log_username
             })
           })
         } else {
@@ -73,9 +77,60 @@ export class LoginPage {
     )
   }
 
-      clear() {
-        this.nativeStorage.clear()
+  clear() {
+    this.nativeStorage.clear()
+  }
+
+  signUp() {
+    const { sign_username, sign_password, sign_password_confirmation } = this.user.value
+    if (sign_username == "" || sign_password == "" || sign_password_confirmation == "") {
+      let alert = this.alertCtrl.create({
+        title: 'Error',
+        subTitle: 'All fields must be filled',
+        buttons: ['Dismiss']
+      })
+      alert.present()
+    } else {
+      if (sign_password === sign_password_confirmation) {
+        this.nativeStorage.getItem(sign_username).then(data => {
+          let alert = this.alertCtrl.create({
+            title: 'Error',
+            subTitle: 'You are already registered',
+            buttons: ['Dismiss']
+          })
+          alert.present();
+        }, error => {
+          this.nativeStorage.setItem(sign_username, {
+            sign_password
+          }).then(() => {
+            let alert = this.alertCtrl.create({
+              title: 'Register successful',
+              subTitle: 'Welcome! You are now registered & logged in',
+              buttons: ['Dismiss']
+            })
+            alert.present()
+            alert.onDidDismiss(() => {
+              this.session.setUser(sign_username)
+              this.navCtrl.setRoot(HomePage, {
+                sign_username
+              })
+            })
+          },
+            error => {
+              console.error("Error storing item", error)
+            })
+        })
+      } else {
+        let alert = this.alertCtrl.create({
+          title: 'Error',
+          subTitle: `Passwords doesn't match`,
+          buttons: ['Dismiss']
+        })
+        alert.present()
       }
+    }
+  } 
+
   }
 
 
